bb0de5bdc05d2ecba767098868597fac31c4ef96
marioandluigi878 <calvin4084@gmail.com>
2017-03-08 02:19:27 -0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
-set -x
 # SRCDS Container Start Script
 #
 # Copyright (c) 2016 Dane Everitt <dane@daneeveritt.com>
 # MIT Licensed
 # ##
 sleep 3
-if [ "$(pwd)" != "/home/container" ]; then
+if [ "$(pwd)" -ne "/home/container" ]; then
     cd /home/container
 fi
 
+function install-rocketmod {
+	if [ "$SRCDS_GAME" == "Unturned" ]; then
+		if [ ! -f "/home/container/RocketLauncher.exe" ]; then
+			wget "https://api.rocketmod.net/download.unturned-linux.latest.DC24040A-0AB8-4207-B068-3DCE1194CD4A" -O /home/container/rocketmod.zip
+			unzip rocketmod.zip -d . && rm -f rocketmod.zip
+			mv Scripts/*.sh .
+		fi
+	fi
+}
+
 # Download SteamCMD, it is missing
 if [ ! -f "/home/container/steamcmd/steamcmd.sh" ]; then
+	if [ ! -d "/home/container/steamcmd" ]; then
+		mkdir /home/container/steamcmd
+	fi
+	if [ "$SRCDS_GAME" == "Unturned" ]; then
+		if [ ! -d "/home/container/unturned" ]; then
+			mkdir /home/container/unturned
+		fi
+		installdir="/home/container/unturned"
+	fi
     mkdir steamcmd; cd steamcmd
 
     set -x
@@ -28,9 +46,9 @@ if [ ! -f "/home/container/steamcmd/steamcmd.sh" ]; then
 
     echo "Installing requested game, this could take a long time depending on game size and network."
     set -x
-    ./steamcmd.sh +login $SCRDS_USER +force_install_dir /home/container +app_update $SRCDS_APPID +quit
+    ./steamcmd.sh +login $SRCDS_EMAIL +force_install_dir $installdir +app_update $SRCDS_APPID +quit
     set +x
-
+	
     cd /home/container
     mkdir -p .steam/sdk32
     cp -v steamcmd/linux32/steamclient.so .steam/sdk32/steamclient.so
@@ -47,4 +65,5 @@ fi
 
 MODIFIED_STARTUP=`echo $STARTUP | perl -pe 's@{{(.*?)}}@$ENV{$1}@g'`
 echo "./srcds_run ${MODIFIED_STARTUP}"
-./srcds_run $MODIFIED_STARTUP
+#./srcds_run $MODIFIED_STARTUP
+./Unturned.x86_64 -nographics -pei -normal -nosync -pve -players:16 -ip:$SERVER_IP -port:$SERVER_PORT
